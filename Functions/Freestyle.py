5a077cc560081321f7667a9a6da0d98f98491d32
Axil Vigo <axil-pacha@hotmail.com>
2019-05-07 10:43:22 -0400

    Added some functions

--- Functions/Freestyle.py
+++ Functions/Freestyle.py
@@ -4,20 +4,22 @@ def sum(num):
     exemple: num = 3, sum = 6
     '''
     a_sum = 0
-    for x in range(num+1):
+    for x in range(num + 1):
         a_sum += x
     return a_sum
 
-print(sum(2)) #test
 
-def is_triangle(a,b,c):
+print(sum(2))  # test
+
+
+def is_triangle(a, b, c):
     '''
     This function takes 3 lengths and returns if wether or not they can create a triangle
     '''
-    return a+b>c
+    return a + b > c
 
-print(is_triangle(2,3,5)) #test
 
+print(is_triangle(2, 3, 5))  # test
 
 
 def toJadenCase(string):
@@ -28,8 +30,9 @@ def toJadenCase(string):
     x = ' '.join(alist)
     return x
 
+
 astring = 'her name is her'
-print(toJadenCase(astring)) #test
+print(toJadenCase(astring))  # test
 
 
 def getCount(inputStr):
@@ -45,8 +48,10 @@ def getCount(inputStr):
 
     return num_vowels
 
+
 astr = 'Je mappelle sammy'
-print(getCount(astr)) #test
+print(getCount(astr))  # test
+
 
 def find_smallest_int(arr):
     '''
@@ -54,8 +59,10 @@ def find_smallest_int(arr):
     '''
     return min(arr)
 
-alist = [23,45,3,445,765,1]
+
+alist = [23, 45, 3, 445, 765, 1]
 print(find_smallest_int(alist))
+
 
 def no_space(x):
     '''
@@ -63,6 +70,7 @@ def no_space(x):
     '''
     lst = x.split()
     return ''.join(lst)
+
 
 astr = 'The apple is red'
 print(no_space(astr))
@@ -76,7 +84,7 @@ def DNA_strand(dna):
     for letter in dna:
         if letter == 'A':
             c_dna.append('T')
-        elif letter =='T':
+        elif letter == 'T':
             c_dna.append('A')
         elif letter == 'C':
             c_dna.append('G')
@@ -93,20 +101,80 @@ def Descending_Order(num):
     ls = []
     for numb in str(num):
         ls.append(numb)
-    ls.sort(reverse = True) #sorts list in reverse (descending order)
+    ls.sort(reverse=True)  # sorts list in reverse (descending order)
     return int(''.join(ls))
 
+
 print(Descending_Order(1234))
+
 
 def array_diff(a, b):
     '''
     This function subtracts one list from another and returns the result
     '''
-    c =[]
+    c = []
     for x in a:
         if x not in b:
             c.append(x)
     return c
 
-print(array_diff([1,2,3],[2,3]))
 
+print(array_diff([1, 2, 3], [2, 3]))
+
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    else:
+        for x in range(1, num + 1):
+            if num % x == 0:
+                if x == num:
+                    return True
+
+
+print(is_prime(2))
+
+alist = [1, 2, 5, 4, 5]
+
+
+def queue_time(customers, n):
+    '''
+    This customer determines the longest wait time possible on a queue, customers is a list of customers with their respective
+    amount of time taken in the queue, n is the number of checkout tills
+    '''
+    alist = []
+
+    if len(customers) == 0: #if there is no customer
+        return 0
+    elif len(customers) <= n: #if theres less customers than the number of checkout tills (or =)
+        return max(customers)
+    else:
+        for i in range(n):
+            alist.append(customers[i])
+        for i in range(n,len(customers)):
+            alist[alist.index(min(alist))] += customers[i] #The smallest value of alist (smallest amount of time) is incremented with time
+        #of next customer
+        return max(alist) #returns the max amount of time, or total time taken to go through all customers
+
+from random import randint
+def is_valid_IP(strng):
+    '''
+    This function returns wether or not an IPV4 is valid or not
+    '''
+    l = (strng.replace('.', ' ')).split()
+    if len(l) != 4:
+        return False
+    else:
+        for part in l:
+            if int(part) > 255:
+                return False
+            elif int(part) > 0 and '0'+str(randint(0,9)) in strng:
+                return False
+            else:
+                return True
+
+
+ip = '12.255.56.1'
+print(is_valid_IP(ip))
+
+'0'+str(randint(0,9)) in strng
